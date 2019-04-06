1a93791f020f85a9692ebc1dc43377b7299aa9df
Kevin Guzman <kevindanielguzmen98@gmail.com>
2019-04-05 23:45:25 -0500

    Creación de modelo base para Rest.

---
+++ commons/traits/ManagementAdmColumns.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace kevocode\tools\commons\traits;
+
+use yii\behaviors\TimestampBehavior;
+use yii\behaviors\BlameableBehavior;
+use yii\db\Expression;
+
+/**
+ * Trait para la implementación de los comportamientos para el manejo de las columnas de administración.
+ * 
+ * @property boolean $useAdminColumns Define si se usarán o no 
+ * los comportamientos para las columnas de administración.
+ * 
+ * @package app
+ * @subpackage tools\commons\traits
+ * @category traits
+ * 
+ * @author Kevin Daniel Guzman Delgadillo <kevindanielguzmen98@gmail.com>
+ * @version 0.0.1
+ * @since 0.0.0
+ */
+trait ManagementAdmColumns {
+    public $useAdminColumns = true;
+    
+    /**
+     * Define los comportamientos por defecto para el modelo.
+     * 
+     * @return array
+     */
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+        if ($this->useAdminColumns) {
+            $behaviors = array_merge($behaviors, [
+                [
+                    'class' => TimestampBehavior::className(),
+                    'createdAtAttribute' => static::CREATED_AT_COLUMN,
+                    'updatedAtAttribute' => static::UPDATED_AT_COLUMN,
+                    'value' => new Expression('NOW()'),
+                ],
+                [
+                    'class' => BlameableBehavior::className(),
+                    'createdByAttribute' => static::CREATED_BY_COLUMN,
+                    'updatedByAttribute' => static::UPDATED_BY_COLUMN,
+                    'defaultValue' => static::APPLICATION_USER_ID
+                ]
+            ]);
+        }
+        return $behaviors;
+    }
+}
---
+++ rest/Model.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace kevocode\tools\rest;
+
+use yii\behaviors\TimestampBehavior;
+use yii\behaviors\BlameableBehavior;
+use yii\db\Expression;
+use kevocode\tools\commons\traits\ManagementAdmColumns;
+
+/**
+ * Modelo del cual extienden los modelos para el módulo.
+ * 
+ * @property boolean $useAdminColumns Determina si se usará columnas de administración.
+ * 
+ * @package kevocode
+ * @subpackage tools\rest
+ * @category models
+ * 
+ * @author Kevin Daniel Guzmán Delgadillo <kevindanielguzmen98gmail.com>
+ * @version 0.0.1
+ * @since 0.0.0
+ */
+class Model extends \yii\db\ActiveRecord
+{
+    use ManagementAdmColumns;
+
+    const CREATED_AT_COLUMN = 'created_at';
+    const UPDATED_AT_COLUMN = 'updated_at';
+    const CREATED_BY_COLUMN = 'created_by';
+    const UPDATED_BY_COLUMN = 'updated_by';
+    const STATUS_COLUMN = 'record_status';
+    const STATUS_ACTIVE = 'A';
+    const STATUS_INACTIVE = 'I';
+    const APPLICATION_USER_ID = 1;
+
+    /**
+     * Definición de evento de inicialización del modelo
+     */
+    public function init()
+    {
+        parent::init();
+        $this->{static::STATUS_COLUMN} = static::STATUS_ACTIVE;
+    }
+}
